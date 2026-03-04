9c9ccc8a59739f3701d192dda18ebbf5205ee276
gsmisra <gauravmisra.sankar@gmail.com>
2026-03-04 15:54:04 +0800

    Add ESLint config

---
+++ 02StartingProject/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ["react"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react/prop-types": "warn",
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
